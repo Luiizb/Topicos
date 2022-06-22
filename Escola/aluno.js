e8e6487a0e7591a8e46e25b30d1fcbde6769bb71
Luiizb <luizcesar.fonseca@gmail.com>
2022-06-22 13:41:12 -0300

    Final v.UmMilhão

--- Escola/aluno.js
+++ Escola/aluno.js
@@ -1,22 +1,41 @@
-fetch("https://raw.githubusercontent.com/LuizStocco/Json.Alunos/main/Json/Alunos.json")
-.then(response => response.json())
-.then(json =>{
-  const nomeinput =  document.getElementById("Nome")
-  nomeinput.value = json.nome
+fetch("https://raw.githubusercontent.com/LuizStocco/ProjetoEscola.Json/main/data/areaAluno.json")
+  .then(response => response.json())
+  .then(json => {
+    const nomeinput = document.getElementById("nome")
+    nomeinput.append(json.Nome)
+
+
+    createAtividades(json)
+    createProvas(json)
+  })
+
+function createAtividades(json) {
   
+  json.AtividadesExtra.forEach((atividade, index) => {
+    const atividadeEx = document.getElementById("atividades")
+    const atividadeA = document.createElement("a")
+    atividadeA.innerText = atividade.Nome
+    atividadeEx.appendChild(atividadeA)
+  })
+
+}
+
+function createProvas(json) {
   json.Provas.forEach(prova => {
-      const provasul = document.getElementById("provas")
-      const disciplina = document.createElement("p")
-      disciplina.innerText = prova.disciplina
-      const data = document.createElement("p")
-            data.innerText = prova.data 
-        const provali = document.createElement("li")
-        provali.appendChild(disciplina)
-        provali.appendChild(data)     
-        provasul.appendChild(provali)
+    const provasul = document.getElementById("materias")
+    const disciplina = document.createElement("a")
+    disciplina.innerText = prova.disciplina
+    disciplina.onclick = () => preencheCamposDisciplina(prova)
+    provasul.appendChild(disciplina)
   });
 
-})
+}
 
+function preencheCamposDisciplina(prova){
+  const notas = document.getElementById("notasInput")
+  const frequencia = document.getElementById("faltasInput")
+  notas.value = prova.Notas
+  frequencia.value = prova.Frequencia
+}
 
 
